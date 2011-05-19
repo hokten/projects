7bc3bb078403c551a9a52b7eb3fa9e6326505c8d
hokten <patlakbalon@yahoo.com>
2011-05-20 02:39:01 +0300

    HTML olarak düzenlendi

--- index.php
+++ index.php
@@ -1,24 +1,5 @@
-dasd asd asd
-sdfsdf
-dsfsdfs
-d
-dsadd
-as
-dsadf
-Eklendi
-sdfsdf
-sdf
-dfs
-asasd
-das
-
-da
-sa
-
-sdf
-sdffdsd
-fdsf
-sd
-fd
-s
-sdfsdfdsissssssssssssssssssssss
+<html>
+<body>
+<font size="3"></font>
+</body>
+</html>
